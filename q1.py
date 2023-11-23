cc540995b1bbee6143215699a057d89b73c139cf
Racketmins <113955657+Racketmins@users.noreply.github.com>
2023-11-23 13:17:25 +0900

    Add files via upload

--- q1.py
+++ q1.py
@@ -1,8 +1,33 @@
-from socketserver
+import numpy as np
+import operator
 
-class MyRequestHandler(socketsever.StreamRequestHandler):
-    def handle(self):
-        self.wfile.write("%s" % self.rfile.read().decode().encode())
+try:
+    f = open("data.txt")
+    data = f.read()
+    datas = []
+    for line in data:
+        datas.append(line) 
+except FileNotFoundError:
+    print("Not file")
+finally:
+    f.close()
 
-sever = socketsever.TCPServer(("",8888), MyRequestHandler)
-sever.close()
+def createDataSet(datas):
+    group = []
+    labels = []
+    for i in datas:
+        group.append(datas[i][0], datas[i][1], datas[i][2])
+        labels.append(datas[i][3])
+    group = np.array(group)
+    labels = np.array(labels)
+    return group, labels
+
+def autoNorm(dataSet):
+    minValue = dataSet.min(0)
+    maxValue = dataSet.max(0)
+    ranges = maxValue - minValue
+    normDataSet = np.zeros(np.shape(dataSet))
+    m = dataSet.shape[0]
+    norm
+def classify0(inX, group, labels, k):
+
