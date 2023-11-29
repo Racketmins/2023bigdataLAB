72c47695428742bc47087cf4d595162edb803f82
Racketmins <113955657+Racketmins@users.noreply.github.com>
2023-11-29 16:46:06 +0900

    Add files via upload

--- q1.py
+++ q1.py
@@ -2,7 +2,7 @@ import numpy as np
 import operator
 
 try:
-    f = open("data.txt")
+    f = open("datinTestSet.txt")
     data = f.read()
     datas = []
     for line in data:
@@ -28,6 +28,26 @@ def autoNorm(dataSet):
     ranges = maxValue - minValue
     normDataSet = np.zeros(np.shape(dataSet))
     m = dataSet.shape[0]
-    norm
-def classify0(inX, group, labels, k):
+    normDataSet = dataSet - np.tile(minValue, (m,1))
+    normDataSet = normDataSet / np.tile(ranges, (m,1))
+    return normDataSet
 
+def classify0(inX, dataSet, labels, k):
+    dataSetSize = dataSet.shape[0]
+    diffMat = np.tile(inX, (dataSetSize, 1)) - dataSet
+    sqDiffMat = diffMat ** 2
+    distances = sqDiffMat.sum(axis = 1)
+    sortedDistance = distances.argsort()
+    classCount = {}
+    for i in range(k):
+        dLabel = labels[sortedDistance[i]]
+        clasCount[dLabel] = classCount.get(dLabel, 0) + 1
+    sortedClassCount = sorted(classCount.items(), key = operator.itemgetter(1), reverse = True)
+    return sortedClassCount[0][0] 
+
+
+group, labels = createDataSet(datas)
+group = autoNorm(group)
+answer = input()
+answer = answer.split()
+print(classify0(answer, group, labels, 10))        
