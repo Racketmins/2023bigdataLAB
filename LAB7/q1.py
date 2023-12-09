dd880b6ad0ec4350bfbe4baec515c256d03329a9
Racketmins <113955657+Racketmins@users.noreply.github.com>
2023-12-09 14:25:36 +0900

    Add files via upload

--- LAB7/q1.py
+++ LAB7/q1.py
@@ -1,1 +1,13 @@
-print('hello world!')
+class Camera:
+	def __init__(self, pixel, multi):
+		self.pixel = pixel
+		self.multi = multi
+	def takepicture(self):
+		print("찰칵")
+		print("사진이 저장되었습니다.(화소:%d만, 배율:%.1fx)" % (self.pixel, self.multi))
+
+if __name__ == "__main__":
+	canon = Camera(2430, 1.0)
+	canon.takepicture()
+	sony = Camera(2410, 3.0)
+	sony.takepicture()
